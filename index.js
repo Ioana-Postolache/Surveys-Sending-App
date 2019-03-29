ddf9eafa80946e1d8b9f555480ef22f73691524b
Ioana-Postolache <ioana.postolache.zaharia@gmail.com>
2019-03-29 10:40:47 +0200

    Added write to Mongo schema

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
-require("./services/passport");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
+require("./models/User");
+require("./services/passport");
 
 const app = express();
 
 require("./routes/authRoutes")(app);
+
+mongoose.connection.on("open", function(ref) {
+  console.log("Connected to mongo server.");
+});
+
+mongoose.connection.on("error", function(err) {
+  console.log("Could not connect to mongo server!");
+  return console.log(err);
+});
+
+
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
